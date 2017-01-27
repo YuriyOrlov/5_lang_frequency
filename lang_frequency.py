3854e4c72087e629084467d5ab7306d675a3251a
YuriyOrlov <navuchodonsr@yandex.ru>
2017-01-27 13:04:38 +0300

    finished project

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,31 @@
+from sys import argv
+from os import path
+import string
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    if not path.exists(filepath):
+        return None
+    with open(filepath, 'r', encoding='cp1251') as file:
+        raw_file = file.read()
+        text_without_punctuation = raw_file.translate(str.maketrans('', '', string.punctuation))
+        text_without_numbers = text_without_punctuation.translate(str.maketrans('', '', '1234567890'))
+        text_without_spec_signs = text_without_numbers.translate(str.maketrans('', '', '\n\t\r'))
+        words_list = text_without_spec_signs.split()
+        return words_list
 
 
 def get_most_frequent_words(text):
-    pass
+    return Counter(text).most_common(30)
 
 
 if __name__ == '__main__':
-    pass
+    if len(argv) is 1 or len(argv) >= 3:
+        print('\nPlease, specify the file for opening. Example: python pprint_json.py alcomarket.json\n')
+    else:
+        words_list_from_text = load_data(argv[1])
+        most_common_words_list = get_most_frequent_words(words_list_from_text)
+        print('The thirty most frequent words in the text (in descending order)')
+        for num, item in enumerate(most_common_words_list, 1):
+            print('{}. {}'.format(num, item[0]))
